75cd2f6c0662ba30dba9a2d628dcb00c18bf985f
im-dashan <mni.dashan@gmail.com>
2024-06-17 16:03:59 +0800

    更新@Tag注解

--- src/main/java/com/dashan/login/controller/OpenApiController.java
+++ src/main/java/com/dashan/login/controller/OpenApiController.java
@@ -1,5 +1,6 @@
 package com.dashan.login.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.dashan.login.constants.OpenApiConstants.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/swagger")
+@Tag(name = "Swagger测试", description = "测试API，可以忽略")
 public class OpenApiController {
 
     @GetMapping("/hello")
--- src/main/java/com/dashan/login/controller/UsersController.java
+++ src/main/java/com/dashan/login/controller/UsersController.java
@@ -1,17 +1,27 @@
 package com.dashan.login.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.dashan.login.constants.OpenApiConstants.HELLO;
 
+@Tag(name = "用户登录注册", description = "处理用户登录注册功能")
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/api/auth")
 public class UsersController {
 
-    @GetMapping("/hello")
-    public String hello() {
+    @PostMapping("/login")
+    @Operation(summary = "用户登录", description = "通过用户名和密码登录")
+    public String login() {
+        return HELLO;
+    }
+
+    @PostMapping("/register")
+    @Operation(summary = "用户注册", description = "通过邮箱验证注册账号")
+    public String register() {
         return HELLO;
     }
 }
